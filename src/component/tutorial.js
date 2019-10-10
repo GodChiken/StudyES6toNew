49ea5c94dd6690fd6cbcf3d4e5675dcb67194b4e
godchiken <godchiken@naver.com>
2019-10-10 12:40:45 +0900

    #3 React - Hello React 실습

---
+++ src/component/tutorial.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HelloReact(){
+    return (
+        <h1 className="greeting">
+            Hello, React!
+        </h1>
+    );
+}
+export default HelloReact;
