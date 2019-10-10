92f2dd4ffc79aedd419c584c4954a9e84c5c2305
godchiken <godchiken@naver.com>
2019-10-10 17:41:57 +0900

    #3 React - Hello React 실습

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import Tutorial from './component/Tutorial';
 import TickTock from './component/TickTok';
+import Welcome from "./component/props";
 
 ReactDOM.render(<Tutorial />, document.getElementById('tutorial'));
-ReactDOM.render(<TickTock />, document.getElementById('tick'));
+ReactDOM.render(<Welcome name="Bo-Hun"/>,document.getElementById('tick'));
 ReactDOM.render(<App />, document.getElementById('root'));
