f6b5df8df633ce4697697941b07ca9b41e590a31
godchiken <godchiken@naver.com>
2019-10-15 14:29:54 +0900

    이벤트 제어하기 실습

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
 import Tutorial from './component/Tutorial';
 import TickTock from './component/TickTok';
 import Welcome from "./component/props";
 import WelcomeContainer from "./component/componentCombine";
 import Clock from "./component/Clock";
+import Toggle from "./component/Toggle";
 
-ReactDOM.render(<Tutorial />, document.getElementById('tutorial'));
-ReactDOM.render(<Welcome name="Bo-Hun"/>,document.getElementById('tick'));
-ReactDOM.render(<WelcomeContainer/>,document.getElementById('combineComponent'));
-ReactDOM.render(<Clock/>,document.getElementById('Clock'));
+render(<Tutorial/>, document.getElementById('tutorial'));
+render(<Welcome name="Bo-Hun"/>, document.getElementById('tick'));
+render(<WelcomeContainer/>, document.getElementById('combineComponent'));
+render(<Clock/>, document.getElementById('Clock'));
+render(<Toggle/>, document.getElementById('toggle'));
